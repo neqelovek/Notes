8ba236a3d049bc385d6d980d52ed88163dc5a0c2
neqelovek <neqelovek@gmail.com>
2022-01-06 21:52:45 +0300

    Lesson6 finish

---
+++ app/src/main/java/ru/gb/notes/ui/NotesListActivity.java
@@ -0,0 +1,73 @@
+package ru.gb.notes.ui;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import ru.gb.notes.R;
+import ru.gb.notes.data.Constants;
+import ru.gb.notes.data.InMemoryRepoImpl;
+import ru.gb.notes.data.Note;
+import ru.gb.notes.data.Repo;
+import ru.gb.notes.recycler.NotesAdapter;
+
+public class NotesListActivity extends AppCompatActivity implements NotesAdapter.OnNoteClickListener {
+
+    private Repo repository = InMemoryRepoImpl.getInstance();
+    private RecyclerView list;
+    private NotesAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes_list);
+
+        adapter = new NotesAdapter();
+        adapter.setNotes(repository.getAll());
+
+        adapter.setOnNoteClickListener(this);
+
+        list = findViewById(R.id.list);
+        list.setLayoutManager(new LinearLayoutManager(this));
+        list.setAdapter(adapter);
+    }
+
+    @Override
+    public void onNoteClick(Note note) {
+        Intent intent = new Intent(this, EditNoteActivity.class);
+        intent.putExtra(Constants.NOTE, note);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_create) {
+            Intent intent = new Intent(this, EditNoteActivity.class);
+            int id = repository.getAll().size() + 1;
+            Note note = new Note(id, "", "");
+            intent.putExtra(Constants.NOTE, note);
+            repository.create(note);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        adapter.setNotes(repository.getAll());
+        list.setAdapter(adapter);
+    }
+}
